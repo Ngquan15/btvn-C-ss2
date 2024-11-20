33308ff3fc7087fe2d6c5397f5888e5fe44334c6
Ngquan15 <dinhquan1552006@gmail.com>
2024-11-20 23:26:18 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	  int a = 5 , b = 5;
+	  printf("chieu dai hcn la : %d \n",a);
+	  printf("chieu rong hcn la : %d \n",b);
+	  int c = ( a + b ) * 2;
+	  printf("chu vi hcn la : %d \n",c);
+	  int d = a * b;
+	  printf("dien tich hcn la : %d \n",d);
+	
+	return 0;
+} 
