8668d56a8531702dff549e16fafec447d4c2fc68
Ngquan15 <dinhquan1552006@gmail.com>
2024-11-20 23:25:08 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	 // kieu du lieu so nguyen 
+	 int can_nang_80kg;
+	 // kieu du lieu so thuc
+	 float can_nang;
+	 double trong_luong;
+	 // kieu du lieu ki tu 
+	 char so_loung;
+
+	return 0;
+} 
---
+++ bai3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	  int a = 10,b = 2;
+	 printf("so dau tien la : %d \n",a);
+	 printf("so thu hai la : %d \n",b);
+	 int c = a + b;
+	 printf("tong hai so la : %d \n",c);
+	 int d = a - b;
+	 printf("hieu hai so la : %d \n",d);
+	 int e = a * b;
+	 printf("tich hai so la : %d \n",e);
+	 float f = a / b;
+	 printf("thuong hai so la : %.f \n",f);
+	 
+	return 0;
+} 
